922658baf62ecea8f2c8b34ec65b2574d01e3b5c
Alejandro Perdomo González <apergot95@gmail.com>
2019-11-12 16:00:03 +0000

    Sign up form done

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,5 +1,68 @@
 <template>
-    <div>
-        <h1>This is the signup component</h1>
+    <div id="signup">
+    <div class="signup-form">
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="email">Mail</label>
+          <input
+                  type="email"
+                  id="email"
+                  v-model="email">
+        </div>
+        <div class="input">
+          <label for="age">Your Age</label>
+          <input
+                  type="number"
+                  id="age"
+                  v-model.number="age">
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+                  type="password"
+                  id="password"
+                  v-model="password">
+        </div>
+        <div class="input">
+          <label for="confirm-password">Confirm Password</label>
+          <input
+                  type="password"
+                  id="confirm-password"
+                  v-model="confirmPassword">
+        </div>
+        <div class="input">
+          <label for="country">Country</label>
+          <select id="country" v-model="country">
+            <option value="SPN">Spain</option>
+            <option value="UK">United Kingdom</option>
+            <option value="USA">United States</option>
+            <option value="GER">Germany</option>
+          </select>
+        </div>
+        <div>
+          <input type="checkbox" id="terms" v-model="terms">
+          <label for="terms">Accept Terms of Use</label>
+        </div>
+        <div class="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      age: null,
+      password: '',
+      confirmPassword: '',
+      country: 'usa',
+      hobbyInputs: [],
+      terms: false
+    }
+  }
+}
+</script>
